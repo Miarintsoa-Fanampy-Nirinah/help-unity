8d7ba4d44da91245b982e6bb223ce6ac34ae4535
Miarintsoa <23l04l03@gmail.com>
2024-12-21 18:36:01 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Exemple d'animation : au clic sur le bouton "Démarrer"
+document.querySelector('.start-button').addEventListener('click', function() {
+    alert('Le projet démarre !');
+    // Tu peux ajouter d'autres animations ici (par exemple un changement d'écran, un fade-in, etc.)
+});
