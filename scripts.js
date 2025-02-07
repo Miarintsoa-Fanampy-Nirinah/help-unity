c9d8ae6d6e53c2c0948da30a72f5f70cc31ffe64
Miarintsoa <23l04l03@gmail.com>
2025-02-07 11:44:23 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Exemple d'animation : au clic sur le bouton "Démarrer"
-document.querySelector('.start-button').addEventListener('click', function() {
-    alert('Le projet démarre !');
-    // Tu peux ajouter d'autres animations ici (par exemple un changement d'écran, un fade-in, etc.)
-});
